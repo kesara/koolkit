fa57144c005e579ecc37aa9b58d1f3fba46dda55
kesara.rathnayake <kesara.rathnayake@13d76fa2-cc3d-0410-82f8-cd380398883d>
2007-11-20 19:51:55 +0000

    Version 0.1.1
    git-svn-id: https://koolkit.googlecode.com/svn/trunk@11 13d76fa2-cc3d-0410-82f8-cd380398883d

--- crun/config.h
+++ crun/config.h
@@ -5,22 +5,22 @@
 #define PACKAGE "crun"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT ""
+#define PACKAGE_BUGREPORT "http://code.google.com/p/koolkit/"
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "crun"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "crun 0.1"
+#define PACKAGE_STRING "crun 0.1.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "crun"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.1"
+#define PACKAGE_VERSION "0.1.1"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "0.1"
+#define VERSION "0.1.1"
--- crun/main.c
+++ crun/main.c
@@ -25,38 +25,128 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
+/* Global Veriables */
+char *program_name;					/* Program Name */
+char *program_version = "0.1.1";	/* Program Version */
 
-#define MIN	3	/* Minimum number of command line arguments. */
+/* Prototypes */
+void print_usage (void);
+void print_help (void);
+void print_version (void);
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
+	register int n;					/* number of times to execute */
+	register int time;				/* time interval */
+	register char* program;			/* command name */
+	
+	program_name = argv[0];
 
-	register int nTimes;
-	register int time;
-	register char* program;
-
-	if (argc > MIN) {
-		nTimes = atoi(argv[1]);
+	if (argc > 3)
+	{
+		n = atoi(argv[1]);
 		time = atoi(argv[2]);
 		program = argv[3];
 
 		int i;
-		for (i = 0; i < MIN; i++)
+		int cid;
+		
+		for (i = 0; i < 3; i++)
+		{
 			++argv;
+		}
 
-		for(i = 0; i < nTimes; i++) {
-			int cid = fork();
+		for (i = 0; i < n; i++)
+		{
+			cid = fork();
 
 			if (cid == 0)
-				execvp(program, argv);
+			{
+				execvp (program, argv);
+			}
 
-			if (i+1 != nTimes && time > 0)
-				sleep(time);
+			if (i+1 != n && time > 0)
+			{
+				sleep (time);
+			}
+		}
+		
+		waitpid (cid, NULL, NULL);
+	}
+	else if (argc == 2)
+	{
+		char *help = "--help";
+		char *version = "--version";
+		
+		if (strcmp (argv[1], help) == 0)
+		{
+			print_help ();
+			exit (0);
+		}
+		else if (strcmp (argv[1], version) == 0)
+		{
+			print_version ();
+			exit (0);
+		}
+		else
+		{
+			fprintf (stderr, "%s: invalid arguments\n", program_name);
+			print_usage ();
+			exit (-1);
 		}
 	}
-	else {
-		printf("usage: crun <No of Times> <Time Interval> <Program Namge> [Program Parameters]\n");
-		return (-1);
+	else
+	{
+		fprintf (stderr, "%s: invalid arguments\n", program_name);
+		print_usage ();
+		exit (-1);
 	}
-	return (0);
+	
+	exit (0);
+}
+
+/*
+ * print_usage - print usage message
+ */
+void print_usage (void)
+{
+	
+	fprintf (stderr,
+			 "usage: %s no_of_times time_interval program \n",
+			 program_name);
+}
+
+/*
+ * print_help - print help on the program
+ */
+void print_help (void)
+{
+	print_usage ();
+	fprintf (stdout,
+			 "Executes an user defined programs multiple times with a user ");
+	fprintf (stdout,
+			 " defined time interval.\n");
+	fprintf (stdout,
+			 "  --help\tprint help\n");
+	fprintf (stdout,
+			 "  --version\tprint version\n");
+	fprintf (stdout,
+			 "\nReport bugs to http://code.google.com/p/koolkit/issues/list\n");
+}
+
+/*
+ * print_version - print program version information
+ */
+void print_version (void)
+{
+	fprintf (stdout, "%s %s (beta)\n", program_name, program_version);
+	fprintf (stdout, "Copyright (C) 2007 Kesara Nanayakkara Rathnayake\n");
+	fprintf (stdout, "This is free software. You may redistribute copies of it");
+	fprintf (stdout, "the terms of\n");
+	fprintf (stdout, "the GNU General Public License ");
+	fprintf (stdout, "<http://www.gnu.org/licenses/gpl.html>.\n");
+	fprintf (stdout, "There is NO WARRANTY, to the extent permitted by law.\n");
+	fprintf (stdout, "\nWritten by Kesara Nanayakkara Rathnayake.\n");
 }
